fd1ae40c649c4e3ac8c4ce10c721bf7574dd6e6d
y-mak <makado.y2527@gmail.com>
2021-09-03 20:39:48 +0900

    アソシエーション コメントアウト

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,6 +12,6 @@ class User < ApplicationRecord
   validates :birthday, presence: true
   validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
 
-  has_many :items
-  has_many :records
+  #has_many :items
+  #has_many :records
 end
