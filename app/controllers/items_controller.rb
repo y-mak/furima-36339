84724779718ef53b98d1a9b78ec71b15706c9b63
y-mak <makado.y2527@gmail.com>
2021-09-03 23:19:49 +0900

    ルーティング・コントローラーの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,19 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    Item.create(item_params)
+  end
+
+
+  private
+  def item_params
+    parems.require(:item).permit(:name, :comment, :category_id, :status_id, :fee_id, :area_id, :days_id, :price, :user)
+  end
+
 end
