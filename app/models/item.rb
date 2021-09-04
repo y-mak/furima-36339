d27cff9dc364f05c3afff1a46aac5808a5e23a3f
y-mak <makado.y2527@gmail.com>
2021-09-04 12:00:08 +0900

    ActiveHashの実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,11 +1,18 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :status
+  belongs_to :fee
+  belongs_to :area
+  belongs_to :day
+
   validates :name, presence: true
   validates :comment, presence: true
-  validates :category_id, presence: true
-  validates :status_id, presence: true
-  validates :fee_id, presence: true
-  validates :area_id, presence: true
-  validates :days_id, presence: true
+  validates :category_id, numericality: { other_than: 1, message: "can't be blank"} 
+  validates :status_id, numericality: { other_than: 1, message: "can't be blank"} 
+  validates :fee_id, numericality: { other_than: 1, message: "can't be blank"} 
+  validates :area_id, numericality: { other_than: 1, message: "can't be blank"} 
+  validates :days_id, numericality: { other_than: 1, message: "can't be blank"} 
   validates :price, presence: true ,format: { with: /\A[0-9]+\z/ }
   validates :user, presence: true
 
