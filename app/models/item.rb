0f852f9be00c6afae551bf85c47b5000c0ff33c3
y-mak <makado.y2527@gmail.com>
2021-09-04 09:32:00 +0900

    Itemモデル　バリデート記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,16 @@
 class Item < ApplicationRecord
+  validates :name, presence: true
+  validates :comment, presence: true
+  validates :category_id, presence: true
+  validates :status_id, presence: true
+  validates :fee_id, presence: true
+  validates :area_id, presence: true
+  validates :days_id, presence: true
+  validates :price, presence: true ,format: { with: /\A[0-9]+\z/ }
+  validates :user, presence: true
+
   belongs_to :user
   has_one :record
   has_one_attached :image
-  
+
 end
