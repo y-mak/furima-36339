9bd42265aa40e22b5fb2f37b993a4e6dfaff0772
y-mak <makado.y2527@gmail.com>
2021-09-05 17:54:04 +0900

    JavaScript実装

--- app/javascript/calc.js
+++ app/javascript/calc.js
@@ -1,5 +1,12 @@
-function post (){
-  console.log("イベント発火");
-};
-
-window.addEventListener('load', post);
+window.addEventListener('load', function(){
+  const priceInput = document.getElementById("item-price");
+    priceInput.addEventListener('input', function(){
+    console.log("Input action")
+    console.log(this.value)
+      const priceInputValue =  this.value
+      const commission = document.getElementById("add-tax-price");
+      commission.innerHTML = Math.round(priceInputValue * 0.1);
+      const getProfit = document.getElementById("profit");
+      getProfit.innerHTML = Math.round(priceInputValue - Math.round(priceInputValue * 0.1 ))
+  })
+});
