220b9d7067476e793a0924e37f11e890afc909de
y-mak <makado.y2527@gmail.com>
2021-09-11 21:45:45 +0900

    レビュー修正1

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :edit]
+  before_action :authenticate_user!, only: [:new, :edit, :destroy]
   before_action :move_to_index, only: [:edit, :destroy]
   
   def index
