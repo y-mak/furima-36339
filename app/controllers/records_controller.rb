67b14a305b8387227b9a5208d05189d83273a66f
y-mak <makado.y2527@gmail.com>
2021-09-15 23:11:43 +0900

    Formオブジェクトの作成

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -1,4 +1,24 @@
 class RecordsController < ApplicationController
   def index
+    @item = Item.find(params[:item_id])
+    @record_address = RecordAddress.new
   end
+
+  def create
+    @item = Item.find(params[:item_id])
+    @record_address = RecordAddress.new(record_params)
+    if @record_address.valid?
+      @record_address.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def record_params
+    params.require(:record_address).permit(:postal_code, :area_id, :municipalities, :house_number, :building, :tel, :record_id).merge(user_id: current_user.id, item_id: params[:item_id])
+  end
+
 end
--- app/models/record_address.rb
+++ app/models/record_address.rb
@@ -1,4 +1,4 @@
-class RecodrAddress
+class RecordAddress
   include ActiveModel::Model
   attr_accessor :user_id, :item_id, :postal_code, :area_id, :municipalities, :house_number, :building, :tel, :record_id
 
@@ -7,10 +7,11 @@ class RecodrAddress
     validates :area_id, numericality: {other_than: 1, message: "can't be blank"} 
     validates :municipalities
     validates :house_number
-    validates :tel, numericality: {only_integer: true, greater_than_or_equal_to: 10, less_than_or_equal_to: 11, message: 'is invalid'}
+    #validates :tel, numericality: {only_integer: true, greater_than_or_equal_to: 10, less_than_or_equal_to: 11, message: 'is invalid'}
+    validates :tel, format: {with: /\A\d{10,11}\z/, message: 'is invalid'}
     validates :item_id
     validates :user_id
-    validates :record_id
+    #validates :record_id
   end
 
   def save
