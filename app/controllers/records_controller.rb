9219d4f75affb4669a808e2417ddd8c72389d02e
y-mak <makado.y2527@gmail.com>
2021-09-16 23:34:41 +0900

    決済機能のサーバーサイド実装

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -8,6 +8,7 @@ class RecordsController < ApplicationController
     @item = Item.find(params[:item_id])
     @record_address = RecordAddress.new(record_params)
     if @record_address.valid?
+      pay_item
       @record_address.save
       redirect_to root_path
     else
@@ -18,7 +19,16 @@ class RecordsController < ApplicationController
   private
 
   def record_params
-    params.require(:record_address).permit(:postal_code, :area_id, :municipalities, :house_number, :building, :tel, :record_id).merge(user_id: current_user.id, item_id: params[:item_id])
+    params.require(:record_address).permit(:postal_code, :area_id, :municipalities, :house_number, :building, :tel, :record_id).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+  end
+
+  def pay_item
+    Payjp.api_key = "sk_test_cb3b987327069d8d91082a05"
+      Payjp::Charge.create(
+        amount: @item[:price],  # 商品の値段
+        card: record_params[:token],
+        currency: 'jpy'
+      )
   end
 
 end
--- app/models/record_address.rb
+++ app/models/record_address.rb
@@ -1,16 +1,16 @@
 class RecordAddress
   include ActiveModel::Model
-  attr_accessor :user_id, :item_id, :postal_code, :area_id, :municipalities, :house_number, :building, :tel, :record_id
+  attr_accessor :user_id, :item_id, :postal_code, :area_id, :municipalities, :house_number, :building, :tel, :record_id, :token
 
   with_options presence: true do
     validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
     validates :area_id, numericality: {other_than: 1, message: "can't be blank"} 
     validates :municipalities
     validates :house_number
-    #validates :tel, numericality: {only_integer: true, greater_than_or_equal_to: 10, less_than_or_equal_to: 11, message: 'is invalid'}
     validates :tel, format: {with: /\A\d{10,11}\z/, message: 'is invalid'}
     validates :item_id
     validates :user_id
+    validates :token
     #validates :record_id
   end
 
